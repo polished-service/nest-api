923f288e58921a215bb2dab0789bd3f65984d2ad
Nikita Lukashik <n.lukashik@vsemayki.ru>
2024-10-08 02:00:19 +0400

    feat: add nestjs/config for environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CustomConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [CustomConfigModule],
   controllers: [],
   providers: [],
 })
---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+const config = () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+});
+
+export default config;
