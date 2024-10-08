0ba546d06380ffb709ce6e0e0688230798707b7a
Nikita Lukashik <n.lukashik@vsemayki.ru>
2024-10-08 19:59:07 +0400

    feat: added prisma orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { CustomConfigModule } from './config/config.module'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-    imports: [CustomConfigModule],
+    imports: [CustomConfigModule, PrismaModule],
     controllers: [],
     providers: [],
 })
