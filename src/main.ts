da572ab95b82b5808ae87d69262807419e6a8c34
Nikita Lukashik <n.lukashik@vsemayki.ru>
2024-10-09 05:05:25 +0400

    feat: added global validation for body

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     const configService = app.get<ConfigService>(ConfigService)
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+        })
+    )
 
     const port = configService.get<number>('port')
 
