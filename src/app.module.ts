bb5dcd49de4244954130487fcf2973b492d44d90
Nikita Lukashik <n.lukashik@vsemayki.ru>
2024-10-13 03:27:53 +0400

    feat: added cache module with redis handle

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
-import { CustomConfigModule } from './config/config.module'
 import { PrismaModule } from './prisma/prisma.module'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
+import { RedisModule } from './redis/redis.module'
+import { CustomConfigModule } from './config/config.module'
 
 @Module({
-    imports: [CustomConfigModule, PrismaModule, UsersModule, AuthModule],
+    imports: [CustomConfigModule, PrismaModule, UsersModule, AuthModule, RedisModule],
     controllers: [],
     providers: [],
 })
---
+++ src/redis/redis.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common'
+import { CacheModule } from '@nestjs/cache-manager'
+import { CustomConfigModule } from '../config/config.module'
+import { ConfigService } from '@nestjs/config'
+import * as redisStore from 'cache-manager-redis-store'
+import { RedisConfig } from '../config/config'
+
+@Module({
+    imports: [
+        CacheModule.registerAsync({
+            imports: [CustomConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                store: redisStore,
+                host: configService.get<RedisConfig>('redis').host,
+                port: configService.get<RedisConfig>('redis').port,
+            }),
+        }),
+    ],
+    exports: [CacheModule],
+})
+export class RedisModule {}
