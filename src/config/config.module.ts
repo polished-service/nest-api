db50973e724231fe8abd512aff7412ba441b0b0a
Nikita Lukashik <n.lukashik@vsemayki.ru>
2024-10-13 03:27:07 +0400

    fix: global handle for ConfigModule in right way

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,11 +1,11 @@
-import { Global, Module } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import config from './config'
 
-@Global()
 @Module({
     imports: [
         ConfigModule.forRoot({
+            isGlobal: true,
             load: [config],
             expandVariables: true,
         }),
